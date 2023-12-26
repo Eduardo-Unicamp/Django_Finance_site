e01d950b4da52f787c7eca490b5589a3b10ff39f
Edu <eduardojuniorsilvasaldanhacmd@gmail.com>
2023-12-26 11:13:27 -0300

    .

--- projeto_Finance_site/projeto_Finance_site/urls.py
+++ projeto_Finance_site/projeto_Finance_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_Finance_site import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]
