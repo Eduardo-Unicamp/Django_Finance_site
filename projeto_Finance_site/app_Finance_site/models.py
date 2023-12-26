f433e9da11e9925153b35638b7b12a43fadd1b2b
Edu <eduardojuniorsilvasaldanhacmd@gmail.com>
2023-12-26 16:01:35 -0300

    codigo sql criado atraves do arquivo models.py, comandos makemigrations e migrate, agora falta ir no views e passar conteudos do formulario para o banco criado

--- projeto_Finance_site/app_Finance_site/models.py
+++ projeto_Finance_site/app_Finance_site/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Create_database(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.TextField(max_length=255)
+    description = models.TextField(max_length=255)
+    value = models.FloatField()
